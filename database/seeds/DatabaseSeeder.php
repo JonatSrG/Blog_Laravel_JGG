048878b9aadf45fc931a0fdfe752b69786dd989c
JonatSrG <JonatSrG>
2021-02-19 20:03:44 -0600

    configuramos el factory y el post para la base de datos y tablas

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        App\User::create([
+            'name' => 'Galindo Jonathan',
+            'email' => 'galindo@admin.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        factory(App\Post::class, 24)->create();
     }
 }
