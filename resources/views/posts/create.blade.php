da4fa9f6fa06fe55819a8f336584b19d29309354
JonatSrG <JonatSrG>
2021-02-19 22:59:25 -0600

    Se termina lo visual de la view de create

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Crear Articulos</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,32 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <form 
+                        action="{{ route('posts.store') }}"
+                        method="POST"
+                        enctype="multipart/form-data"
+                    >
+                        <div class="form-group">
+                            <label>Titulo *</label>
+                            <input type="text" name="title" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Imagen</label>
+                            <input type="file" name="file">
+                        </div>
+                        <div class="form-group">
+                            <label>Contenido *</label>
+                            <textarea name="body" rows="6" class="form-control" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label>Contenido embebido</label>
+                            <textarea name="iframe" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            @csrf
+                            <input type="submit" value="Enviar" class="btn btn-sm btn-primary">
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
